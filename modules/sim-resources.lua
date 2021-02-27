207dd9452c5c6dbfdf0589886317e3bb6eba252a
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-02-27 18:10:15 +0100

    Extract giveBonusToKings

--- modules/sim-resources.lua
+++ modules/sim-resources.lua
@@ -48,24 +48,26 @@ local function countTotalMassEco(playerTables)
     return total
 end
 
+local function giveBonusToKings(playerTables, amountOfKings)
+    local totalMassEco = countTotalMassEco(playerTables)
+
+    for _, player in playerTables do
+        if player.isKing then
+            local amount = (1.0 / amountOfKings) * 0.10 * totalMassEco
+            GetArmyBrain(player.strArmy):GiveResource('MASS', amount)
+        end
+    end
+end
+
 function Tick(playerTables)
 
     local amountOfContestants = countContestants(playerTables)
     local amountOfKings = countKings(playerTables)
-    local totalMassEco = countTotalMassEco(playerTables)
+
 
     -- if there are any kings
     if amountOfKings > 0 then
-        for k, player in playerTables do 
-            if player.isKing then 
-                -- determine the amount
-                local amount = (1.0 / amountOfKings) * 0.10 * totalMassEco
-    
-                -- provide resources
-                local brain = GetArmyBrain(player.strArmy)
-                brain:GiveResource('MASS', amount)
-            end
-        end
+        giveBonusToKings(playerTables, amountOfKings)
     else 
         -- else, if there are any contestants
         if amountOfContestants > 0 then
